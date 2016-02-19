eee2c5ff86cd7927da5bcc3909f0f338a0ffdd90
Souler <soulerhyd@gmail.com>
2016-02-19 12:57:44 +0100

    Bye bye arrow functions... you will be missed :(

--- test/test.js
+++ test/test.js
@@ -2,37 +2,37 @@ const should           = require('should')
 const Waterline        = require('waterline')
 const WaterlineLighter = require('../')
 
-describe('WaterlineLighter', () => {
-    it('should be a function', () => {
+describe('WaterlineLighter', function() {
+    it('should be a function', function() {
         should(WaterlineLighter).be.a.Function()
     })
 
-    it('should be available via Waterline.Lighter', () => {
+    it('should be available via Waterline.Lighter', function() {
         should(Waterline.Lighter).be.a.Function()
             .and.be.equal(WaterlineLighter)
     })
 
-    it('should be available via Waterline.initialize', () => {
+    it('should be available via Waterline.initialize', function() {
         should(Waterline.initialize).be.a.Function()
             .and.be.equal(WaterlineLighter)
     })
 
-    it('should be rejected if no arguments given', () => {
+    it('should be rejected if no arguments given', function() {
         WaterlineLighter().should.be
             .rejectedWith(TypeError, {
                 message: 'First argument must be the path to the models directory or an object with a dir attribute'
             })
     })
 
-    describe('when using a String as first argument', () => {
+    describe('when using a String as first argument', function() {
 
-        it('should be rejected if the directory doesn\'t exist', () => {
+        it('should be rejected if the directory doesn\'t exist', function() {
             WaterlineLighter('./models-ne').should.be.rejected()
         })
 
-        it('should be fullfilled if a valid path', () => {
+        it('should be fullfilled if a valid path', function() {
             return WaterlineLighter('./models')
-            .then((wl) => {
+            .then(function(wl) {
                 should.exist(wl)
                 should.exist(wl.orm)
                 should.exist(wl.config)
@@ -41,9 +41,9 @@ describe('WaterlineLighter', () => {
             })
         })
 
-        it('collections and connections should be available in global object', () => {
+        it('collections and connections should be available in global object', function() {
             return WaterlineLighter('./models')
-            .then((wl) => {
+            .then(function(wl) {
                 should.exist(global.collections)
                 global.collections.should.be.equal(wl.collections)
                 should.exist(global.connections)
@@ -52,8 +52,8 @@ describe('WaterlineLighter', () => {
         })
     })
 
-    describe('when using an Object as first argument', () => {
-        it('should be rejected if no dir attribute', () => {
+    describe('when using an Object as first argument', function() {
+        it('should be rejected if no dir attribute', function() {
             WaterlineLighter({}).should.be
                 .rejectedWith(TypeError, {
                     message: 'First argument must be the path to the models directory or an object with a dir attribute'
@@ -61,12 +61,12 @@ describe('WaterlineLighter', () => {
         })
     })
 
-    describe('#middleware', () => {
-        it('should be a function', () => {
+    describe('#middleware', function() {
+        it('should be a function', function() {
             should(WaterlineLighter.middleware).be.a.Function()
         })
 
-        it('should return a function', () => {
+        it('should return a function', function() {
             should(WaterlineLighter.middleware()).be.a.Function()
         })
     })
